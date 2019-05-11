34efb0702ce7794ecfd0bda689deb377b7925897
Robert Hutzel <amoebob@fastmail.fm>
2019-05-11 18:54:35 -0400

    test: simplify context necessary for the ingest handling test

--- src/main/java/com/rhutzel/librarylink/server/config/SearchRouter.java
+++ src/main/java/com/rhutzel/librarylink/server/config/SearchRouter.java
@@ -3,7 +3,6 @@ package com.rhutzel.librarylink.server.config;
 import static org.springframework.web.reactive.function.server.RequestPredicates.accept;
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 
-import com.rhutzel.librarylink.server.controller.IngestHandler;
 import com.rhutzel.librarylink.server.controller.SearchHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,18 +12,12 @@ import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
 @Configuration
-public class Router {
+public class SearchRouter {
 
     @Bean
     public RouterFunction<ServerResponse> searchRoutes(SearchHandler handler) {
         return RouterFunctions
                 .route(GET("/search").and(accept(MediaType.APPLICATION_JSON)), handler::search);
-    }
-
-    @Bean
-    public RouterFunction<ServerResponse> ingestRoutes(IngestHandler handler) {
-        return RouterFunctions
-                .route(GET("/ingest").and(accept(MediaType.APPLICATION_JSON)), handler::ingest);
     }
 
 }
--- src/test/java/com/rhutzel/librarylink/server/IngestTest.java
+++ src/test/java/com/rhutzel/librarylink/server/IngestTest.java
@@ -1,14 +1,16 @@
 package com.rhutzel.librarylink.server;
 
+import com.rhutzel.librarylink.server.config.IngestRouter;
+import com.rhutzel.librarylink.server.controller.IngestHandler;
 import com.rhutzel.librarylink.server.service.IngestService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Mono;
@@ -16,8 +18,8 @@ import reactor.core.publisher.Mono;
 import java.util.Map;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-@AutoConfigureWebTestClient
+@ContextConfiguration(classes = {IngestHandler.class, IngestRouter.class})
+@WebFluxTest
 public class IngestTest {
 
     @MockBean
